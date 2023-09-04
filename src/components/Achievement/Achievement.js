6a6c2f72c5002a3ac445df1512c0001925c0207e
mr-vikash <mrvikash700@gmail.com>
2023-09-04 10:12:25 +0530

    adding some styling in achievements section

--- src/components/Achievement/Achievement.js
+++ src/components/Achievement/Achievement.js
@@ -12,11 +12,15 @@ function Achievement() {
         <>
             {achievementData.achievements.length > 0 && (
                 <div className="achievement" id="achievement" style={{backgroundColor: theme.secondary}}>
-                <div className="achievement-body">
-                    <h1 style={{color: theme.primary}}>Achievements</h1>
-                    <h4 style={{color:theme.tertiary}}>{achievementData.bio}</h4>
-                </div>
+                <div className="experience-image" style={{marginTop:"200px"}}>
+                     <img src={theme.expimg} alt="" />
+                 </div>
+                
                 <div className="achievement-cards">
+                <div className="achievement-body">
+                    <h1 style={{color: theme.primary,paddingLeft:"400px"}}>Achievements</h1>
+                    {/* <h4 style={{color:theme.tertiary}}>{achievementData.bio}</h4> */}
+                </div>
                     {achievementData.achievements.map(achieve => ( 
                         <AchievementCard 
                         key={achieve.id}
