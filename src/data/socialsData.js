93cfff34f6e68bc8616933b2a748d0ee8f3ca1d1
mr-vikash <mrvikash700@gmail.com>
2023-09-05 14:05:17 +0530

    addling proper link of my facebook and twitter account

--- src/data/socialsData.js
+++ src/data/socialsData.js
@@ -1,10 +1,10 @@
 export const socialsData = {
     github: 'https://github.com/mr-vikash',
-    facebook: 'https://www.facebook.com/home.php',
+    facebook: 'https://www.facebook.com/simran.khatun.566',
     linkedIn: 'https://www.linkedin.com/in/its-vikash-gupta',
     instagram: 'https://www.instagram.com/mrvikash700',
     codepen: 'https://codepen.io/',
-    twitter: 'https://twitter.com/home',
+    twitter: 'https://twitter.com/mrvikash700',
     reddit: 'https://www.reddit.com/user/',
     blogger: 'https://www.blogger.com/',
     medium: 'https://medium.com/@',
