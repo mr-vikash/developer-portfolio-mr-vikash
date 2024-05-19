39a1d03e414b597eeaeba374cec27d28b3087d56
mr-vikash <mrvikash700@gmail.com>
2024-05-20 00:00:54 +0530

    Resume updated

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
-    description1: "My name is  Vikash Gupta. I am a web designer and Software developer and currently I am working as Software Engineer Trainee in Unicosmosoft Private Limited, Hydrabad India",
+    description1: "My name is  Vikash Gupta. I am a web designer and Software developer and currently I am working as Software Engineer at Unihyr Labs Private Limited",
     description2: "During the day I work as a Software developer at a local agency and in the evening I  work on my skills and utilize the time to built my own products.I love to learn and explore new areas.",
     image: 1
 }
