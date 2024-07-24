711312a2f3c18e4555843a4ace3fc1e2416225e3
mr-vikash <mrvikash700@gmail.com>
2024-07-24 10:20:57 +0530

    adjusting some spacing

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -3,8 +3,8 @@ export const experienceData = [
         id: 1,
         company: 'Unihyr Labs Private Limited, india',
         jobtitle: 'Software Engineer',
-        startYear: 'July-2023',
-        endYear: 'Present'
+        startYear: 'July-2023 ',
+        endYear: ' Present'
     },
     // {
     //     id: 2,
@@ -24,14 +24,14 @@ export const experienceData = [
         id: 4,
         company: 'Unihyr Labs Private Limited, india',
         jobtitle: 'Software Engineer Intern',
-        startYear: 'January-2023',
-        endYear: 'June-2022'
+        startYear: 'January-2023 ',
+        endYear: ' June-2022'
     },
     {
         id: 5,
         company: 'Akurailabs, Lucknow',
         jobtitle: 'Java Intern',
-        startYear: 'May-2020',
-        endYear: 'June-2020'
+        startYear: 'May-2020 ',
+        endYear: ' June-2020'
     },
 ]
