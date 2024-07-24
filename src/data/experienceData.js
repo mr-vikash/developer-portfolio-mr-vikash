8cd85b94de360aeaf82a25a24cb017d9d2eedd5c
mr-vikash <mrvikash700@gmail.com>
2024-07-24 10:25:28 +0530

    code fix for spacing

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -4,7 +4,7 @@ export const experienceData = [
         company: 'Unihyr Labs Private Limited, india',
         jobtitle: 'Software Engineer',
         startYear: 'July-2023 ',
-        endYear: ' Present'
+        endYear: ' to Present'
     },
     // {
     //     id: 2,
@@ -25,13 +25,13 @@ export const experienceData = [
         company: 'Unihyr Labs Private Limited, india',
         jobtitle: 'Software Engineer Intern',
         startYear: 'January-2023 ',
-        endYear: ' June-2022'
+        endYear: ' to June-2022'
     },
     {
         id: 5,
         company: 'Akurailabs, Lucknow',
         jobtitle: 'Java Intern',
         startYear: 'May-2020 ',
-        endYear: ' June-2020'
+        endYear: ' to June-2020'
     },
 ]
